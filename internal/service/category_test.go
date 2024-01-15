af47e9335a28e8e69a4a2eacf0aec95ff770c8e5
Hidayat Hamir <hidayat.03@erajaya.com>
2024-01-15 13:08:00 +0700

    feat: add api http delete category

--- internal/service/category_test.go
+++ internal/service/category_test.go
@@ -84,3 +84,72 @@ func Test_categoryService_Create(t *testing.T) {
 		})
 	}
 }
+
+func Test_categoryService_Delete(t *testing.T) {
+	type fields struct {
+		categoryRepository *repository.MockCategory
+	}
+	type args struct {
+		ctx        context.Context
+		categoryID int64
+	}
+	tests := []struct {
+		name    string
+		mock    func(f fields)
+		args    args
+		wantErr bool
+	}{
+		{
+			name: "success delete category by id",
+			mock: func(f fields) {
+				f.categoryRepository.EXPECT().Delete(nil, int64(100)).Return(nil)
+			},
+			args: args{
+				ctx:        nil,
+				categoryID: 100,
+			},
+			wantErr: false,
+		},
+		{
+			name: "fail delete category by id",
+			mock: func(f fields) {
+				f.categoryRepository.EXPECT().Delete(nil, int64(100)).Return(assert.AnError)
+			},
+			args: args{
+				ctx:        nil,
+				categoryID: 100,
+			},
+			wantErr: true,
+		},
+		{
+			name: "category_id is not valid",
+			mock: func(f fields) {},
+			args: args{
+				ctx:        nil,
+				categoryID: -100,
+			},
+			wantErr: true,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			ctrl := gomock.NewController(t)
+			defer ctrl.Finish()
+
+			f := fields{
+				categoryRepository: repository.NewMockCategory(ctrl),
+			}
+			tt.mock(f)
+
+			cs := &categoryService{
+				categoryRepository: f.categoryRepository,
+			}
+			err := cs.Delete(tt.args.ctx, tt.args.categoryID)
+			if tt.wantErr {
+				assert.NotNil(t, err)
+			} else {
+				assert.Nil(t, err)
+			}
+		})
+	}
+}
